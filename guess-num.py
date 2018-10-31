15ee395db943bf2cbbdab90632abd0a24620f54c
Chunjie Lu <lvchunjie00@gmail.com>
2018-10-31 00:42:18 -0400

    add cound“

--- guess-num.py
+++ guess-num.py
@@ -1,12 +1,19 @@
 import random
-r = random.randint(1, 100)
+start = input('请输入目标数字最小值:')
+end = input('请输入目标数字最大值:')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
+count = 0
 while True:
 	answer = input('请输入数字:')
 	answer = int(answer)
+	count += 1
 	if answer == r:
-		print('你猜中了!')
+		print('你猜中了!共猜了', count ,'次')
 		break
 	elif answer > r:
 		print('中奖号码比', answer ,'要小')
 	elif answer < r:
-		print('中奖号码比', answer ,'要大')
+		print('中奖号码比', answer ,'要大') 
+	print('你猜了', count , '次')
